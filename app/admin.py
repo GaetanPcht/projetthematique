079af5bf8e00fc40920294df34a8b9540ac754a4
tmeteyer <80888436+tmeteyer@users.noreply.github.com>
2022-02-21 11:43:35 +0100

    Accès BDD + import de class

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import *
 
-# Register your models here.
+
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ('agency_id', 'agency_name','agency_url', 'agency_timezone', 'agency_lang', )
+admin.site.register(Agency, AgencyAdmin)
+
+class CalendarAdmin(admin.ModelAdmin):
+    list_display = ('service_id', 'monday','tuesday', 'wednesday','thursday', 'friday', 'saturday', 'sunday', 'start_date', 'end_date', )
+admin.site.register(Calendar, CalendarAdmin)
--- projetthematique/settings.py
+++ projetthematique/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'projetthematique.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -107,17 +108,17 @@ AUTH_PASSWORD_VALIDATORS = [
 
 TIME_ZONE = 'Europe/Paris'
 LANGUAGE_CODE = 'fr-FR'
-USE_I18N = True
 
+USE_I18N = True
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
