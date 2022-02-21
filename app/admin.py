ecc5166a4756eeded1750739584352d41f58362b
tmeteyer <80888436+tmeteyer@users.noreply.github.com>
2022-02-21 14:56:39 +0100

    Toutes les class ont été créées

--- app/admin.py
+++ app/admin.py
@@ -14,3 +14,30 @@ class Calendar_datesAdmin(admin.ModelAdmin):
     list_display = ('service_id', 'date','exception_type',)
 admin.site.register(Calendar_dates, Calendar_datesAdmin)
 
+class RoutesAdmin(admin.ModelAdmin):
+    list_display = ('route_id','agency_id','route_short_name','route_long_name','route_desc','route_type','route_url','route_color','route_text_color',)
+admin.site.register(Routes, RoutesAdmin)
+
+class ShapesAdmin(admin.ModelAdmin):
+    list_display = ('shape_id','shape_pt_lon','shape_pt_lat','shape_pt_sequence',)
+admin.site.register(Shapes, ShapesAdmin)
+
+class StopsAdmin(admin.ModelAdmin):
+    list_display = ('stop_id','stop_name','stop_desc','stop_lat','stop_lon','zone_id','stop_url','location_type','parent_station','wheelchair_boarding',)
+admin.site.register(Stops, StopsAdmin)
+
+class Stop_extensionsAdmin(admin.ModelAdmin):
+    list_display = ('object_id','object_system','object_code',)
+admin.site.register(Stop_extensions, Stop_extensionsAdmin)
+
+class Stop_timesAdmin(admin.ModelAdmin):
+    list_display = ('trip_id','arrival_time','departure_time','stop_id','stop_sequence','stop_time_desc','pickup_type','drop_off_type',)
+admin.site.register(Stop_times, Stop_timesAdmin)
+
+class TransfersAdmin(admin.ModelAdmin):
+    list_display = ('from_stop_id','to_stop_id','transfer_type','min_transfer_time',)
+admin.site.register(Transfers, TransfersAdmin)
+
+class TripsAdmin(admin.ModelAdmin):
+    list_display = ('route_id','service_id','trip_id','trip_headsign','trip_short_name','direction_id','block_id','wheelchair_accessible','bikes_allowed','trip_desc','shape_id',)
+admin.site.register(Trips, TripsAdmin)
--- app/models.py
+++ app/models.py
@@ -24,8 +24,71 @@ class Calendar(models.Model):
     end_date = models.CharField(max_length=8, default='YYYYDDMM')
 
 class Calendar_dates(models.Model):
-    service_id = models.ForeignKey('Calendar', on_delete=models.CASCADE)
+    service_id = models.ForeignKey('Calendar', on_delete=models.CASCADE) #LIEN VERS calendar
     date = models.CharField(max_length=8, default='YYYYDDMM')
     exception_type = models.CharField(max_length=1, default='1') 
 
+class Routes(models.Model):
+    route_id = models.CharField(primary_key=True, max_length=140)
+    agency_id = models.ForeignKey('Agency', on_delete=models.CASCADE) #LIEN VERS AGENCY
+    route_short_name = models.CharField(max_length=140, default='')
+    route_long_name = models.CharField(max_length=140, default='')
+    route_desc = models.CharField(max_length=140, default='')
+    route_type = models.CharField(max_length=140, default='')
+    route_url = models.CharField(max_length=140, default='')
+    route_color = models.CharField(max_length=6, default='')
+    route_text_color = models.CharField(max_length=6, default='')
+
+class Shapes(models.Model):
+    shape_id = models.CharField(primary_key=True, max_length=140)
+    shape_pt_lon = models.CharField(max_length=140, default='')
+    shape_pt_lat = models.CharField(max_length=140, default='')
+    shape_pt_sequence = models.CharField(max_length=1, default='0')
+
+class Stops(models.Model):
+    stop_id = models.CharField(primary_key=True,  max_length=140)
+    stop_name = models.CharField(max_length=140, default='')
+    stop_desc = models.CharField(max_length=140, default='')
+    stop_lat = models.CharField(max_length=140, default='')
+    stop_lon = models.CharField(max_length=140, default='')
+    zone_id = models.CharField(max_length=140, default='')
+    stop_url = models.CharField(max_length=140, default='')
+    location_type = models.CharField(max_length=1, default='')
+    parent_station = models.CharField(max_length=140, default='')
+    wheelchair_boarding = models.CharField(max_length=1, default='')
+
+class Stop_extensions(models.Model):
+    object_id = models.CharField(primary_key=True, max_length=140)
+    object_system = models.CharField(max_length=140, default='')
+    object_code = models.CharField(max_length=140, default='')
+
+class Transfers(models.Model):
+    # Magouille, foreign key avec un nom différent
+    from_stop_id = models.ForeignKey('Stops', related_name='stop_id_from', on_delete=models.CASCADE, default='') #LIEN VERS STOPS
+    # Magouille, foreign key avec un nom différent
+    to_stop_id = models.ForeignKey('Stops', related_name='stop_id_to', on_delete=models.CASCADE, default='') #LIEN VERS STOPS 
+    transfer_type = models.CharField(max_length=1, default='2')
+    min_transfer_time = models.CharField(max_length=3, default='')
+
+class Trips(models.Model):
+    route_id = models.ForeignKey('Routes', on_delete=models.CASCADE) #LIEN VERS ROUTES
+    service_id = models.ForeignKey('Calendar', on_delete=models.CASCADE) #LIEN VERS CALENDAR
+    trip_id = models.CharField(primary_key=True,  max_length=140)    
+    trip_headsign = models.CharField(max_length=140, default='')
+    trip_short_name = models.CharField(max_length=140, default='')
+    direction_id  = models.CharField(max_length=140, default='') #ID qui ne pointe nulpart ?
+    block_id = models.CharField(max_length=140, default='') #ID qui ne pointe nulpart ?
+    wheelchair_accessible = models.CharField(max_length=1, default='')
+    bikes_allowed =models.CharField(max_length=1, default='')
+    trip_desc = models.CharField(max_length=140, default='')
+    shape_id = models.ForeignKey('Shapes', on_delete=models.CASCADE) #LIEN VERS SHAPES
     
+class Stop_times(models.Model):
+    trip_id = models.ForeignKey('Trips', on_delete=models.CASCADE, default='DEFAULT') #LIEN VERS ROUTES
+    arrival_time = models.TimeField(default='00:00:00')
+    departure_time = models.TimeField(default='00:00:00')
+    stop_id = models.ForeignKey('Stops', on_delete=models.CASCADE, default='DEFAULT') #LIEN VERS AGENCY
+    stop_sequence = models.CharField(max_length=140, default='')
+    stop_time_desc = models.CharField(max_length=1, default='')
+    pickup_type = models.CharField(max_length=1, default='')
+    drop_off_type = models.CharField(max_length=1, default='')
